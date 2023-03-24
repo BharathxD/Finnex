cf949d305f7fd9c4bc83ab3f0847542fb825714e
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-03-24 21:25:43 +0530

    Some code refinements

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,11 @@ import { Box, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import { themeSettings } from "./theme";
 import { useMemo } from "react";
 import { Route, Routes } from "react-router-dom";
-import { useState } from "react";
 import Navbar from "./components/Layout/Navbar";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 const App = () => {
   const theme = useMemo(() => createTheme(themeSettings), []);
-  const [selected, setSelected] = useState<boolean>();
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -16,7 +15,7 @@ const App = () => {
         <Box width={"100%"} height={"100%"} padding={"1rem 2rem 4rem 2rem"}>
           <Navbar />
           <Routes>
-            <Route path={"/"} element={<div>Dashboard Page</div>} />
+            <Route path={"/"} element={<Dashboard />} />
             <Route
               path={"/predictions"}
               element={<div>Predictions Page</div>}
---
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,11 @@
+import { useTheme } from "@mui/material";
+
+const Dashboard = () => {
+  const { palette } = useTheme();
+  return (
+    <>
+      <h1>Dashboard</h1>
+    </>
+  );
+};
+export default Dashboard;
